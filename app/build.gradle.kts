5f917c8161026af523c57175d611d48a9e23c6ea
Dmitry Kotov <40573791+dmitriykotov333@users.noreply.github.com>
2024-07-25 00:35:32 +0300

    update app.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,7 @@
 plugins {
     id("kotdev.android.application")
     id("kotdev.android.application.compose")
-    id("kotlin-kapt")
-    alias(libs.plugins.dagger.hilt)
-//    alias(libs.plugins.android.application)
-//    alias(libs.plugins.jetbrains.kotlin.android)
-//    alias(libs.plugins.compose.compiler)
-//    alias(libs.plugins.dagger.hilt)
-    kotlin("plugin.serialization") version "2.0.0"
+    id("kotdev.android.hilt")
     alias(libs.plugins.compose.compiler)
 }
 
@@ -53,9 +47,6 @@ android {
     buildFeatures {
         compose = true
     }
-//    composeOptions {
-//        kotlinCompilerExtensionVersion = "1.5.14"
-//    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -64,41 +55,17 @@ android {
 
 }
 composeCompiler {
-   // enableStrongSkippingMode = true
+    enableStrongSkippingMode = true
 
     reportsDestination = layout.buildDirectory.dir("compose_compiler")
 }
 dependencies {
     implementation(project(":core"))
-   implementation(project(":core_ui"))
+    implementation(project(":core_ui"))
     implementation(project(":navigation"))
     implementation(project(":trading:data"))
- //   implementation(project(":service"))
-    implementation(libs.android.hilt)
-    kapt(libs.android.hilt.compiler)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.android.coil.compose)
-    implementation(libs.android.navigation)
-    implementation(libs.android.navigation.ui)
-    implementation(libs.android.coroutine)
-    implementation(libs.android.collections.immutable)
-    implementation(libs.android.lifecycle.viewmodel)
-    implementation(libs.android.viewmodel.savedstate)
-    implementation(libs.android.lifecycle.runtime)
-    kapt(libs.android.lifecycle.compiler)
-    implementation(libs.android.chart)
-    implementation(libs.serialization.json)
-    implementation(libs.android.room.paging)
-    implementation(libs.android.paging.runtime)
-    implementation(libs.android.ktor.core)
-    implementation(libs.android.ktor.json)
-    implementation(libs.android.ktor.serialization)
-    implementation(libs.android.ktor.serialization.json)
-    implementation(libs.android.ktor.client)
-    implementation(libs.android.ktor.negotiation)
-    implementation(libs.android.ktor.logging)
-    implementation(libs.android.appwrite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
